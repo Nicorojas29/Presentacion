3b4c64519b39d69016ba3030e46db5759df2577b
Nicorojas29 <nicolassrojas.29@gmail.com>
2023-03-20 20:27:11 -0300

    primera version del servidor

--- src/app.js
+++ src/app.js
@@ -1,1 +1,15 @@
-console.log ("hola mundo");
+
+const express = require ('express'); // trae la funcion para ejecutar el codigo 
+
+const app = express (); // ejecuta la funcion que trae del codigo y el resultado lo guarda en app
+
+const PORT = 3001;
+
+app.get ('/api', ( request , res) =>
+{ 
+      res.json({mensaje: 'Hola mundo'}).status(200) // atributo es mensaje - hola mundo es el valor 
+}); // cuando alguien busque  http:
+//Localhost:3001/api le tiene que salir el mensaje de arriba 
+
+app.listen (PORT, () => { //listen pone la app a escuchar 
+    console.log('estoy escuchando el puerto:' + PORT) })
